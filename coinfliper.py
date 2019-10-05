1bba7d8125bdc2d41c986ae9f199277d997b0177
Juanjo <juanjo.morell7@gmail.com>
2019-10-05 16:04:26 +0200

    First State

---
+++ coinfliper.py
@@ -0,0 +1,6 @@
+import random, os, time, datetime, argparse
+    #random: This module implements pseudo-random number generator
+    #os: This module provides a portable way of using operating system dependent funcionality
+    #time: This module provides various time-related functions.
+    #datetime: This module supplies classes for manipulating dates and times.
+    #argsparse: This module makes it easy to write user-friendly command line interfaces.
